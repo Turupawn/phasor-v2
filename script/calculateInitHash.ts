437cfdb3147af9226643a920f33f36f9aebc05e2
Fuzzb0t <christopherpagnusatt@gmail.com>
2025-12-20 14:42:36 -0600

    Add foundry

---
+++ script/calculateInitHash.ts
@@ -0,0 +1,11 @@
+// scripts/getPoolInitCodeHash.ts
+// You need to find where is the UniswapV3Pool bytecode
+import { bytecode } from "../packages/core/build/UniswapV2Pair.json"
+import { keccak256 } from "ethers";
+
+function getPoolInitCodeHash() {
+ const COMPUTED_INIT_CODE_HASH = keccak256(bytecode.object) 
+ console.log(COMPUTED_INIT_CODE_HASH) 
+}
+
+getPoolInitCodeHash()
