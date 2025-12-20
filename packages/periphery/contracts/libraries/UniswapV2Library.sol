e3e42c6dc7006e2e54538bc35ce57029a03325d6
Fuzzb0t <christopherpagnusatt@gmail.com>
2025-12-20 15:11:38 -0600

    Adding cannon

--- packages/periphery/contracts/libraries/UniswapV2Library.sol
+++ packages/periphery/contracts/libraries/UniswapV2Library.sol
@@ -21,7 +21,7 @@ library UniswapV2Library {
                 hex'ff',
                 factory,
                 keccak256(abi.encodePacked(token0, token1)),
-                hex'96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f' // init code hash
+                hex'99ac59bd068349b1070308e76b3b2170e22ee37112436e62b3e1058ad8eb33b0' // init code hash
             ))));
     }
 
--- script/calculateInitHash.ts
+++ script/calculateInitHash.ts
@@ -1,6 +1,6 @@
 // scripts/getPoolInitCodeHash.ts
 // You need to find where is the UniswapV3Pool bytecode
-import { bytecode } from "../packages/core/build/UniswapV2Pair.json"
+import { bytecode } from "../out/UniswapV2Pair.sol/UniswapV2Pair.json"
 import { keccak256 } from "ethers";
 
 function getPoolInitCodeHash() {
